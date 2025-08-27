0ec5c2e0a677f26d13d00ad3fa669838f80621cd
Hklug <henriqueklug@gmail.com>
2025-08-26 23:41:51 -0300

    hands on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
-        />
-      </head>
       <body className={`${libreBaskerville.className}`}>{children}</body>
     </html>
   );
