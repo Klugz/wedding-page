8d9420170abb64063198b87e83228ed8716933a2
Hklug <henriqueklug@gmail.com>
2025-08-27 20:16:32 -0300

    webkit change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,6 @@ import "./globals.css";
 
 // const libertinusMono = Libertinus_Mono({ weight: ["400"], subsets: ['latin'] });
 
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-  viewportFit: "cover",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
