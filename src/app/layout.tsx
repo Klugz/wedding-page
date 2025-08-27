d9e03b32c98f38bc5d5ceb4db0950e8c1d8176df
Hklug <henriqueklug@gmail.com>
2025-08-26 22:20:44 -0300

    add new metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import { libreBaskerville } from "../styles/fonts/fonts";
+import { Metadata } from "next";
 import "./globals.css";
 
 // const libertinusMono = Libertinus_Mono({ weight: ["400"], subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  viewport: "width=device-width, initial-scale=1.0",
+};
 
 export default function RootLayout({
   children,
