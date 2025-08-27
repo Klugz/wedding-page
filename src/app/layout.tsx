c723f32cb20ff095858d85e63147d2901787c1e1
Hklug <henriqueklug@gmail.com>
2025-08-26 22:38:05 -0300

    more iphone fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
       </head>
       <body className={`${libreBaskerville.className}`}>{children}</body>
     </html>
